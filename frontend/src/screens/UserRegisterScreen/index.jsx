47653b5b3f5f9082d97ae8ffd4f1a2da9b302ea2
lidifaria <falecom@lidifaria.com>
2023-05-03 10:11:15 -0300

    add: criando novo registro de usuário no backend a partir do componente UserRegisterScreen

--- frontend/src/screens/UserRegisterScreen/index.jsx
+++ frontend/src/screens/UserRegisterScreen/index.jsx
@@ -1,38 +1,46 @@
-import './usersScreen.css';
+import './userRegisterScreen.css';
 import Header from '../../components/Header';
-import UserCard from '../../components/UserCard';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import api from '../../api/api';
 
-function UsersScreen(){
+function UserRegisterScreen(){
 
-    const [users, setUsers] = useState([]);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+ 
 
-    useEffect(()=>{
-        async function getUsers(){
-            const response = await api.get('/users');
-            setUsers(response.data);
-        }
+    async function handleSubmit(event){
+        event.preventDefault();
 
-        getUsers();
-    },[]);
+        await api.post('/users',{
+          name,
+          email,
+        })
 
-    return(
+        setName('');
+        setEmail('');
+
+        alert('Usuário cadastrado');
+
+      }
+    
+      return (
         <>
-            <Header title="Usuários cadastrados"/>
-            <main>
-                {users.map(user=> 
-                        <UserCard 
-                            key={user.id}
-                            id={user.id} 
-                            name={user.name} 
-                            email={user.email}
-                        /> 
-                )}
-            </main>
+          <Header title="Cadastro de usuário"/>
+          <main>
+            <form onSubmit={handleSubmit}>
+              <label>Nome</label>
+              <input type="text" value={name} onChange={event=>setName(event.target.value)}/>
+    
+              <label>Email</label>
+              <input type="email" value={email} onChange={event=>setEmail(event.target.value)}/>
+    
+              <button type="submit">Cadastrar</button>
+            </form>        
+          </main>
         </>
-    );
-
+      );
+    
 }
 
-export default UsersScreen;
+export default UserRegisterScreen;
